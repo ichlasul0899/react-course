244c5a51431cb2cf9f06dbd692680a774433e6b8
ichlasulamalgit <id.ichlasulamal@gmail.com>
2020-10-23 21:37:07 +0700

    Adding use effect hooks

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Contact from '../component/Contact';
-import Portfolio from '../component/Portfolio';
 
 const ContactPage = () => {
   return (
     <>
       <Contact />
-      <Portfolio />
     </>
   );
 };
--- src/pages/DetailPortfolioPage.js
+++ src/pages/DetailPortfolioPage.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Portfolio from '../component/Portfolio';
 
 const DetailPortfolioPage = () => {
   const { id } = useParams();
+  const [image, setImage] = useState();
+  const [title, setTitle] = useState();
+  const [desc, setDesc] = useState();
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/posts/${id}`)
+      .then((response) => { return response.json(); })
+      .then((post) => {
+        setTitle(post.title);
+        setImage(post.srcImage);
+        setDesc(post.desc);
+      });
+  }, []);
+
   return (
     <>
-      <Portfolio from="detail" id={id} />
+      <div className="container">
+        <div className="row">
+          <h3 className="center light">
+            Detail
+            {title}
+          </h3>
+          <div className="col m4 s12">
+            <div className="card">
+              <div className="card-image">
+                <img src={image} className="materialboxed" alt={title} />
+                <span className="card-title">{title}</span>
+              </div>
+              <div className="card-content">
+                <p>{desc}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
